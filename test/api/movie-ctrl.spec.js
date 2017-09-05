6c00b06cffbe308dd45bfec87250901b00871458
Idan Asraf <idan.asraf@sap.com>
2017-09-05 16:58:21 +0300

    fixed issue with docker failing to npm install silently

---
+++ test/api/movie-ctrl.spec.js
@@ -0,0 +1,12 @@
+const sinon = require("sinon");
+const chai = require("chai");
+const sinonChai = require("sinon-chai");
+const request = require("supertest");
+const server = require("../../src/server");
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+describe("MovieController", function() {
+    describe("")
+});
