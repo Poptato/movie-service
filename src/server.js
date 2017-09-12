64702e5b60f61dbc3f6efcaef4c9b161d2fe7db7
Idan Asraf <idan.asraf@sap.com>
2017-09-12 17:11:01 +0300

    Added docs through raml

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
-const Express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
+const {join} = require("path");
 
 const InfoController = require("./controller/info-ctrl");
 const MovieController = require("./controller/movie-ctrl");
 const CelebrityController = require("./controller/celeb-ctrl");
 const OscarController  = require("./controller/oscar-ctrl");
 
-let server = new Express();
+let server = new express();
 let port = process.env.PORT || 4000;
 
 server.use(bodyParser.json());
@@ -15,6 +16,7 @@ server.use("/info", InfoController);
 server.use("/movies", MovieController);
 server.use("/celebs", CelebrityController);
 server.use("/oscars", OscarController);
+server.use("/docs", express.static(join(__dirname, "../docs")));
 
 server.listen(port, () => {
    console.log(`Movie service is running on port ${port}`);
