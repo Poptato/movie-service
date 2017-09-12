7c439c9a184f85c6e3beb2f8ff79ec52f358ee57
Idan Asraf <idan.asraf@sap.com>
2017-09-12 13:14:22 +0300

    forgot await on table delete

--- test/assets/db-init.js
+++ test/assets/db-init.js
@@ -5,36 +5,39 @@ const Oscar = require("../../src/model/oscar");
 
 class DbInitializer {
     async init() {
-        try {
-            // Drop existing tables.
-            await Promise.all([
-                thinky.r.db("test").table("Celebrity").delete().run(),
-                thinky.r.db("test").table("Movie").delete().run(),
-                thinky.r.db("test").table("Oscar").delete().run()
-            ]);
-        } finally {
-            // Re-create & populate tables.
-            this.celebs = await this.createCelebs();
-            this.movies = await this.createMovies();
-            this.oscars = await this.createOscars();
-        }
+        // Re-create & populate tables.
+        this.celebs = await this.createCelebs();
+        this.movies = await this.createMovies();
+        this.oscars = await this.createOscars();
 
         console.info("Populated database...");
     }
 
     async createCelebs() {
-        let mockCelebs = require("./mock-celebs.json");
-        return await Promise.all(mockCelebs.map(c => Celebrity(c).save()));
+        try {
+            await thinky.r.db("test").table("Celebrity").delete().run();
+        } finally {
+            let mockCelebs = require("./mock-celebs.json");
+            return await Promise.all(mockCelebs.map(c => Celebrity(c).save()));
+        }
     }
 
     async createMovies() {
-        let mockMovies = require("./mock-movies.json");
-        return await Promise.all(mockMovies.map(m => Movie(m).save()));
+        try {
+            await thinky.r.db("test").table("Movie").delete().run();
+        } finally {
+            let mockMovies = require("./mock-movies.json");
+            return await Promise.all(mockMovies.map(m => Movie(m).save()));
+        }
     }
 
     async createOscars() {
-        let mockOscars = require("./mock-oscars.json");
-        return await Promise.all(mockOscars.map(o => Oscar(o).save()));
+        try {
+            await thinky.r.db("test").table("Oscar").delete().run();
+        } finally {
+            let mockOscars = require("./mock-oscars.json");
+            return await Promise.all(mockOscars.map(o => Oscar(o).save()));
+        }
     }
 }
 
